055f506635bc3f82cb149005ecb14699b5cb5dce
yutoyoshida <yoshida.yuto.23@shizuoka.ac.jp>
2025-01-11 17:59:37 +0900

    feat: HTMLがDjangoに表示可能

--- app/BookReview/urls.py
+++ app/BookReview/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from BookReview import views  # views モジュールをインポート
 
 urlpatterns = [
     path('admin/', admin.site.urls),
