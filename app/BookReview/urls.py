ba06019c536339d6165f2f0a72c96cf38ff54dd7
yutoyoshida <yoshida.yuto.23@shizuoka.ac.jp>
2025-01-23 19:55:20 +0900

    feat: お問い合わせページ

--- app/BookReview/urls.py
+++ app/BookReview/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),  # トップページ
     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
     path('product/<uuid:product_id>/', views.product_detail, name='product_detail'),  # 商品詳細ページ
     path('login/', views.login_view, name='login'),  # ログインページ
     path('logout/', views.logout_view, name='logout'),  # ログアウトページ
